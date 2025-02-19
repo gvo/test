bc7c84135643c48b3308579a804a5b3a4aff6d9e
George Vo <george@voge.ca>
2025-02-19 20:31:09 +0800

    Add files via upload

---
+++ test_pipe.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+
+class Pipe:
+    class Valves(BaseModel):
+        MODEL_ID: str = Field(default="")
+
+    def __init__(self):
+        self.valves = self.Valves()
+
+    def pipe(self, body: dict):
+        # Logic goes here
+        print(self.valves, body)  # This will print the configuration options and the input body
+        return "Hello, World!"
